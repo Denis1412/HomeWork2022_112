34d3d1096071867a7bdc7232ad1d72e5d0bd8283
Denis <server.20@mail.ru>
2022-04-11 12:59:10 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+     public static int Age;
+     public static String Name;
+     public static String Pol;
+     public static int id;
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
